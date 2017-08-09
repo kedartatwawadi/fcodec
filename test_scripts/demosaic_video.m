07e3b4504f56769c10536470dc27d4503f0135c7
kedartatwawadi <ktatwawadi@gmail.com>
2017-08-09 14:09:15 -0700

    cleaned the demosaicing code a bit

--- test_scripts/demosaic_video.m
+++ test_scripts/demosaic_video.m
@@ -11,11 +11,14 @@ for id=3:1:num_images
     filename = dirObj(id).name;
     filepath = strcat('raw_images/',filename);
     dest_name = strcat('demosic_images/demosaic_',filename);
+    
+    % Preprocessing the file, as the red component has really low color values
+    red_calibration_val = 2.0
     I = imread(filepath);
     [w h] = size(I);
     for y = 2:2:w
         for x = 1:2:h
-            I(y,x) = I(y,x)*2;
+            I(y,x) = I(y,x)*red_calibration_val;
         end
     end
     J = demosaic(I,'gbrg');
